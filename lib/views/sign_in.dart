7e5300ae9068a77ccc94e40ca03174be68c3462b
Emmanuel Uzodike <emmanueluzodikecb@gmail.com>
2022-06-28 00:01:54 -0500

    Bug fixed, app bar created

--- lib/views/sign_in.dart
+++ lib/views/sign_in.dart
@@ -1,21 +1,24 @@
 import 'package:flutter/material.dart';
-
-//import '../widgets/widget.dart';
-//import 'package:flutter_chat_app_v2/widgets/widget.dart';
+import 'package:flutter_chat_app_v2/widgets/widget.dart';
 
 class SignIn extends StatefulWidget {
   const SignIn({Key? key}) : super(key: key);
 
   @override
-  State<SignIn> createState() => _nameState();
+  State<SignIn> createState() => _SignInState();
 }
 
-class _nameState extends State<SignIn> {
+class _SignInState extends State<SignIn> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: appBarMain(context),
+      //appBar: AppBar(title: Image.asset("assests/images/logo.png", height: 50)),
+      //appBar: appBarMain(context),
+      //appBar: AppBarMain.build(),
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(50.0),
+        child: appBarMain(context),
+      ),
     );
   }
-  
 }
--- lib/widgets/widget.dart
+++ lib/widgets/widget.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
 
-Widget appBarMain(BuildContext context){
+Widget appBarMain(BuildContext context) {
   return AppBar(
-    title: Image.asset("assets/images/logo.png"),
+
+    title: Image.asset("assests/images/logo.png", height: 50,),
   );
 }
+
+/*class AppBarMain extends StatelessWidget {
+  const AppBarMain({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar();
+  }
+}*/
