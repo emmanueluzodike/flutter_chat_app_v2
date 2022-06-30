7201f1d62ae21b5cebc65bed7bcafc036c9d8512
Emmanuel Uzodike <emmanueluzodikecb@gmail.com>
2022-06-30 01:20:58 -0500

    Fixed app not starting issue

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_chat_app_v2/views/sign_in.dart';
 import 'package:flutter_chat_app_v2/views/sign_up.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
